131db9d756a7efe0710133b2f4783de1a84b0774
Shimokawa Takahiro <takahiro.1828@gmail.com>
2015-04-09 23:38:10 +0900

    メニュー画面を追加する

---
+++ app/src/main/java/androhi/com/triedmaterialdesignapp/StartActivity.java
@@ -0,0 +1,64 @@
+package androhi.com.triedmaterialdesignapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+
+public class StartActivity extends ActionBarActivity implements AbsListView.OnItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        String[] menuStringArray = getResources().getStringArray(R.array.menu_strings);
+        ArrayAdapter<String> adapter =
+                new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menuStringArray);
+        ListView menuList = (ListView) findViewById(R.id.menu_list);
+        menuList.setAdapter(adapter);
+        menuList.setOnItemClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_start, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        switch (position) {
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+        }
+    }
+}
