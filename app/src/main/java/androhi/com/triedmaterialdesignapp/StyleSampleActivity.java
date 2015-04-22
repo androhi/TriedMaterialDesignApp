e1a25cd3258aa9ac245563ca562434d6e72efaac
Shimokawa Takahiro <takahiro.1828@gmail.com>
2015-04-23 02:32:23 +0900

    Add appcompat widget

--- app/src/main/java/androhi/com/triedmaterialdesignapp/StyleSampleActivity.java
+++ app/src/main/java/androhi/com/triedmaterialdesignapp/StyleSampleActivity.java
@@ -3,7 +3,10 @@ package androhi.com.triedmaterialdesignapp;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatAutoCompleteTextView;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.View;
+import android.widget.ArrayAdapter;
 
 
 public class StyleSampleActivity extends AppCompatActivity implements View.OnClickListener {
@@ -14,6 +17,16 @@ public class StyleSampleActivity extends AppCompatActivity implements View.OnCli
         setContentView(R.layout.activity_style_sample);
 
         findViewById(R.id.v7alert_dialog_button).setOnClickListener(this);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_dropdown_item_1line,
+                new String[]{"Android JellyBean", "Android KitKat", "Android Lollipop", "Android M"});
+        ((AppCompatAutoCompleteTextView) findViewById(R.id.appcompat_autocomplete_text))
+                .setAdapter(adapter);
+
+        AppCompatSpinner spinner = (AppCompatSpinner) findViewById(R.id.appcompat_spinner);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 
     @Override
